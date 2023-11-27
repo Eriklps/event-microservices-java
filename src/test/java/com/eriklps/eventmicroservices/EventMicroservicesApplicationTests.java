d0a12c45ecd27fd4d54ca156a5caf62b80450ba2
Eriklps <erikdslopes@gmail.com>
2023-11-27 11:25:14 -0300

    Initial commit

---
+++ src/test/java/com/eriklps/eventmicroservices/EventMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eriklps.eventmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
